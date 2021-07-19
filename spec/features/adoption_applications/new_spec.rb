8f94958605cbbc8382783fe436a2a07b1eee8fa9
Michael Abbott <michael@mjabbottdesign.com>
2021-07-18 19:16:11 -0700

    Add start adopt application form not completed test; story #24

--- spec/features/adoption_applications/new_spec.rb
+++ spec/features/adoption_applications/new_spec.rb
@@ -16,5 +16,19 @@ RSpec.describe 'adoption application creation' do
       new_app_id = AdoptionApplication.last.id
       expect(current_path).to eq("/adoption_applications/#{new_app_id}")
     end
+
+    it 'can not submit with empty form fields' do
+      visit '/adoption_applications/new'
+
+      click_on 'Submit'
+
+      expect(page).to have_content("Name can't be blank")
+      expect(page).to have_content("Street address can't be blank")
+      expect(page).to have_content("City can't be blank")
+      expect(page).to have_content("State can't be blank")
+      expect(page).to have_content("Zip code can't be blank")
+      expect(page).to have_button('Submit')
+      expect(current_path).to eq('/adoption_applications/new')
+    end
   end
 end
