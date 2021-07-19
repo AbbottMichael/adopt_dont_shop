403f0082f4f78b73f56e9232b3749c87330d2a29
Michael Abbott <michael@mjabbottdesign.com>
2021-07-19 16:08:22 -0700

    Refactor layout

--- app/models/adoption_application.rb
+++ app/models/adoption_application.rb
@@ -1,9 +1,10 @@
 class AdoptionApplication < ApplicationRecord
+  has_many :adoption_application_pets
+  has_many :pets, through: :adoption_application_pets
+
   validates :name, presence: true
   validates :street_address, presence: true
   validates :city, presence: true
   validates :state, presence: true
   validates :zip_code, presence: true, numericality: true
-  has_many :adoption_application_pets
-  has_many :pets, through: :adoption_application_pets
 end
