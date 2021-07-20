d1113ae808c359847af09560468389cc4982c99a
Michael Abbott <michael@mjabbottdesign.com>
2021-07-20 14:58:20 -0700

    Add submit an Adoption Application test

--- spec/features/adoption_applications/show_spec.rb
+++ spec/features/adoption_applications/show_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'Adoption application show page' do
   before :each do
-    @adopt_app1 = AdoptionApplication.create!(
+    @adopt_app1 = AdoptionApplication.create(
       name: 'Michael Abbott',
       street_address: '66 Bell St.',
       city: 'Seattle',
       state: 'Wa',
-      zip_code: '98121',
-      status: 'In Progress'
+      zip_code: '98121'
     )
+
     visit "/adoption_applications/#{@adopt_app1.id}"
   end
 
@@ -52,5 +52,50 @@ RSpec.describe 'Adoption application show page' do
     expect(page).to have_content("Pets to adopt: #{pet2.name}")
     expect(@adopt_app1.pets.all).to eq([pet2])
     expect(current_path).to eq("/adoption_applications/#{@adopt_app1.id}")
+  end
+
+  describe 'submit an Adoption Application (To Do #21)' do
+    # As a visitor
+    # When I visit an application's show page
+    # And I have added one or more pets to the application
+    # Then I see a section to submit my application
+    # And in that section I see an input to enter why I would make a good owner for these pet(s)
+    # When I fill in that input
+    # And I click a button to submit this application
+    # Then I am taken back to the application's show page
+    # And I see an indicator that the application is "Pending"
+    # And I see all the pets that I want to adopt
+    # And I do not see a section to add more pets to this application
+    before :each do
+      @shelter = Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
+      @pet1 = Pet.create(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: @shelter.id)
+      @pet2 = Pet.create(name: 'Simba', age: 2, breed: 'Domestic', adoptable: true, shelter_id: @shelter.id)
+    end
+
+    it "displays a section to submit my application after adding 1 or more pets" do
+      expect(@adopt_app1.pets.all.count).to eq(0)
+      expect(page).to_not have_content('Submit your application')
+
+      fill_in('Search for pets:', with: 'Simba')
+      click_on('Search')
+      click_on('Adopt this pet')
+
+      expect(@adopt_app1.pets.all.count).to eq(1)
+      expect(page).to have_content('Submit your application')
+    end
+
+    # it "after submitting application the status is 'Pending'" do
+    #   expect(adopt_app1[:status]).to eq('In Progress')
+    #
+    #   fill_in('Search for pets:', with: 'Simba')
+    #   click_on('Search')
+    #   click_on('Adopt this pet')
+    #   within('#response') do
+    #     fill_in('Response:', with: 'I am great!')
+    #     click_on('Submit my application')
+    #   end
+    #
+    #   expect(adopt_app1[:status]).to eq('Pending')
+    # end
   end
 end
