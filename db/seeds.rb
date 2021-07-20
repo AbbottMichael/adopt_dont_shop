295986ca8b3a1a612631124d9166d01bf19bdab0
Michael Abbott <michael@mjabbottdesign.com>
2021-07-19 19:27:32 -0700

    Update seed destroy_all

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+AdoptionApplicationPet.destroy_all
+Pet.destroy_all
+AdoptionApplication.destroy_all
 Shelter.destroy_all
 
 shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
