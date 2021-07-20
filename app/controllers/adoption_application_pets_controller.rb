7a575062afd4881bc15843db045a57bb7523c373
Michael Abbott <michael@mjabbottdesign.com>
2021-07-19 21:23:32 -0700

    Refactor AdoptionApplicationPetsController to strong params

--- app/controllers/adoption_application_pets_controller.rb
+++ app/controllers/adoption_application_pets_controller.rb
@@ -1,6 +1,12 @@
 class AdoptionApplicationPetsController < ApplicationController
   def create
-    AdoptionApplicationPet.create!(pet_id: "#{params[:pet_id]}", adoption_application_id: "#{params[:adoption_application_id]}")
+    AdoptionApplicationPet.create!(adopt_app_pet_params)
     redirect_to "/adoption_applications/#{params[:adoption_application_id]}"
+  end
+
+  private
+
+  def adopt_app_pet_params
+    params.permit(:pet_id, :adoption_application_id)
   end
 end
